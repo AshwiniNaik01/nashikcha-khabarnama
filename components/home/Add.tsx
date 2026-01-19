09df54b72e4bf4ad652658273e2aab4c1e21a031
ashwiniuttirnaservice <ashwiniuttirnaservice01@gmail.com>
2026-01-19 12:36:21 +0530

    add

--- components/home/Add.tsx
+++ components/home/Add.tsx
@@ -5,10 +5,7 @@ export default function Advertisement({ className }: { className?: string }) {
     <div className={`space-y-6 ${className}`}>
       <div className="py-4">
         <p className="text-center text-xs text-gray-500 mb-1">ADVERTISEMENT</p>
-        <img
-          src="https://dummyimage.com/300x250/0aa/fff&text=Advertisement"
-          className="w-full h-130 object-cover"
-        />
+        <div className="w-full h-130 object-cover  bg-gray-200"></div>
       </div>
     </div>
   );
